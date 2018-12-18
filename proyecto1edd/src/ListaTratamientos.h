54746566094ae884780464b9b5e310dae8132cdd
Shubert <alonzoshubert6991@gmail.com>
2018-12-17 20:55:47 -0800

    Lista de tratamientos definicion

---
+++ proyecto1edd/src/ListaTratamientos.h
@@ -0,0 +1,19 @@
+#ifndef LISTATRATAMIENTOS_H
+#define LISTATRATAMIENTOS_H
+#include <string>
+#include <list>
+#include "NodoTratamiento.h"
+
+class ListaTratamientos{
+private:
+  NodoTratamiento* buscar(std::string s);
+public:
+  NodoTratamiento *inicio,*fin;
+  ListaTratamientos();
+  void insertarAlInicio(Tratamiento t);
+  void insertarAlFinal(Tratamiento t);
+  Tratamiento getTratamiento(std::string id);
+  bool estaVacia();
+  std::list<Tratamiento> getList();
+}
+#endif //LISTATRATAMIENTOS_H
