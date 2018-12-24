e39d2dc9340964b1862784e44981227b084868fe
Shubert <alonzoshubert6991@gmail.com>
2018-12-24 12:41:12 -0600

    Lista years

---
+++ proyecto1edd/src/ListaA.h
@@ -0,0 +1,17 @@
+#ifndef LISTAA_H
+#define LISTAA_H
+#include <string>
+#include <list>
+#include "NodoA.h"
+
+class ListaA{
+private:
+  NodoA* buscar(std::string s);
+public:
+  NodoA *inicio,*fin;
+  ListaA();
+  void insertarAlInicio(Anio a);
+  void insertarAlFinal(Anio a);
+  void insertarOrdenado(Anio a);
+};
+#endif
